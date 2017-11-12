f6d333aa9a70f1cfc7543d18ef8744bfa5dcc511
Julio Ojeda <jcom.94m@gmail.com>
2017-11-12 14:23:07 -0800

    Minior changes

---
+++ src/components/EditExpensePage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const EditExpensePage = () => (
+  <div>
+    This is from my edit expense page component
+  </div>
+);
+
+export default EditExpensePage;
---
+++ src/components/ExpenseDashBoardPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const ExpenseDashBoardPage = () => (
+  <div>
+    This is from my dashboard component
+  </div>
+);
+
+export default ExpenseDashBoardPage;
