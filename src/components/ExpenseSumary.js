5c03da63a0b1ecc5fa6fc932be0c14016acf4996
Julio Ojeda <jcom.94m@gmail.com>
2017-11-29 17:48:21 -0800

    Style Login, Header, Filters

--- src/components/ExpenseSumary.js
+++ src/components/ExpenseSumary.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
+import numeral from 'numeral';
 import selectExpenses from '../selectors/expenses';
 import getExpensesTotal from '../selectors/expenses-total';
-import numeral from 'numeral';
+
 
 export const ExpenseSumary = (props) => (
-  <div>
-    {`Viewing ${props.expenseCount} ${props.expenseCount <= 1 ? 'expense': 'expenses'} totalling ${numeral(props.expensesTotal / 100).format('$0,0.00')}.`}
+  <div className="page-header">
+    <div className="content-container">
+      <h1 className="page-header__title">Viewing <span>{props.expenseCount}</span> {props.expenseCount <= 1 ? 'expense': 'expenses'} totalling <span>{numeral(props.expensesTotal / 100).format('$0,0.00')}</span>.</h1>
+      <div className="page-header__actions">
+        <Link className="button" to="/create">Add Expense</Link>
+      </div>
+    </div>
   </div>
 );
 
