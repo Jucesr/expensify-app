974d129ffcb61146e105c267240162e6b65dc619
Jucesr <jcom.94m@gmail.com>
2017-12-05 23:30:46 -0800

    Server send uncompress bundle js because some problem in firefox

--- server/server.js
+++ server/server.js
@@ -4,28 +4,28 @@ const app = express();
 const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname,'..','public');
 
-// app.use(express.static(publicPath));
+app.use(express.static(publicPath));
+
+// const middleware = (req, res, next) => {
+//     req.url = req.url + '.gz';
+//     res.set('Content-Encoding', 'gzip');
+//     next();
+// };
 //
-const middleware = (req, res, next) => {
-    req.url = req.url + '.gz';
-    res.set('Content-Encoding', 'gzip');
-    next();
-};
-
-app.get('/dist/bundle.js', middleware, (req, res) => {
-  res.sendFile(publicPath + req.url);
-});
-
-app.get('/dist/bundle.js.map', (req, res) => {
-  res.sendFile(publicPath + req.url);
-});
-
-app.get('/dist/styles.css', (req, res) => {
-  res.sendFile(publicPath + req.url);
-});
-app.get('/images/*', (req, res) => {
-  res.sendFile(publicPath + req.url);
-});
+// app.get('/dist/bundle.js', middleware, (req, res) => {
+//   res.sendFile(publicPath + req.url);
+// });
+//
+// app.get('/dist/bundle.js.map', (req, res) => {
+//   res.sendFile(publicPath + req.url);
+// });
+//
+// app.get('/dist/styles.css', (req, res) => {
+//   res.sendFile(publicPath + req.url);
+// });
+// app.get('/images/*', (req, res) => {
+//   res.sendFile(publicPath + req.url);
+// });
 
 
 app.get('*', (req, res) => {
